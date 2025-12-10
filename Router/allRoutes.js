ffae0db76e87683ba347c055a3b667d39c8ba3e8
AlexKidd31 <johndoe@example.comname>
2025-12-10 16:57:05 +0100

    resa et menu

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -9,7 +9,10 @@ export const allRoutes = [
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editpassword", "Changement de mot de passe", "/pages/auth/editpassword.html"),
     new Route("/allResa", "Vos réservation", "/pages/reservations/allResa.html"),
+    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),
+    new Route("/menu", "La carte", "/pages/menu.html"),
 ];
+
 
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
