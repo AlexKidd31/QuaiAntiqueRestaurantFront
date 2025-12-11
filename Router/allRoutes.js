89b7b6ea41edeaded1a474544d04939b6d5cfe4d
AlexKidd31 <johndoe@example.comname>
2025-12-11 18:39:25 +0100

    java

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html", "/js/auth/signup.js"),    
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", "/jse/auth/signup.js"),    
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editpassword", "Changement de mot de passe", "/pages/auth/editpassword.html"),
     new Route("/allResa", "Vos réservation", "/pages/reservations/allResa.html"),
--- jse/auth/signup.js
+++ jse/auth/signup.js
@@ -1,13 +1,14 @@
 
 
+
 const inputNom = document.getElementById("NomInput");
-const inputPreNom = document.getElementById("PrenomInput");
+const inputPrenom = document.getElementById("PrenomInput");
 const inputMail = document.getElementById("EmailInput");
 const inputPassword = document.getElementById("PasswordInput");
 const inputValidationPassword = document.getElementById("ValidatePasswordInput");
 
 inputNom.addEventListener("keyup", validateForm); 
-inputPreNom.addEventListener("keyup", validateForm);
+inputPrenom.addEventListener("keyup", validateForm);
 inputMail.addEventListener("keyup", validateForm);
 inputPassword.addEventListener("keyup", validateForm);
 inputValidationPassword.addEventListener("keyup", validateForm);
